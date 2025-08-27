66fa3403cb8c9eb46b788764f312d051d13acfac
Miloš Jovanović <milos.jovanovic@lombiq.com>
2025-08-28 01:42:08 +0200

    Adding Plural  form to the summary template.

--- OpenWordsPlatform.Common/Services/WordService.cs
+++ OpenWordsPlatform.Common/Services/WordService.cs
@@ -29,4 +29,21 @@ public static class WordService
         }
         return definiteArticle + item.DisplayText;
     }
+    public static string? GetPluralFormOfWordWithArticle(ContentItem item)
+    {
+        string? article = null;
+        string? completeWord = null;
+        if(item.Content.Word.PluralForm.Text != null)
+        {
+            foreach (var widget in item.Content.FlowPart.Widgets)
+            {
+                if (widget["ContentType"] == ContentTypes.ArticlesWidget)
+                {
+                    article = widget["ArticlesPart"]["DefinitePlural"]["Text"] + " ";
+                }
+            }
+            completeWord = article + item.Content.Word.PluralForm.Text;
+        }
+        return completeWord;
+    }
 }
